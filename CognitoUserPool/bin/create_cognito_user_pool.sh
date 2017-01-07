7783594c604ea92d4b72210954f5100eeaf915b1
ganezasan <boss@cheekit.com>
2017-01-07 22:54:06 +0900

    add script for create Cognito User pool and client

---
+++ CognitoUserPool/bin/create_cognito_user_pool.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# Check if the AWS CLI is in the PATH
+found=$(which aws)
+if [ -z "$found" ]; then
+  echo "Please install the AWS CLI under your PATH: http://aws.amazon.com/cli/"
+  exit 1
+fi
+
+# Check if jq is in the PATH
+found=$(which jq)
+if [ -z "$found" ]; then
+  echo "Please install jq under your PATH: http://stedolan.github.io/jq/"
+  exit 1
+fi
+
+# Check dir
+if [ ! -d "CognitoUserPool" ]; then
+  echo "Please execute the command in the parent directory of CognitoUserPool"
+  exit 1
+fi
+
+# Check env
+if [ -z "$STAGE" ]; then
+  echo "Please set STAGE env, $ STAGE=dev sh CognitoUserPool/bin/create_user_pool.sh"
+  exit 1
+fi
+
+cd CognitoUserPool
+
+if [ -d "edit" ]; then
+  rm edit/*
+else
+  mkdir edit
+fi
+
+# files
+userPoolFileName='userPool.json'
+userPoolClientFileName='userPoolClient.json'
+
+USER_POOL_NAME="startupdb-${STAGE}"
+USER_POOL_CLIENT_NAME="startupdb-${STAGE}"
+
+# Create Cognito User Pool
+# find User pool id
+USER_POOL_ID=$(aws cognito-idp list-user-pools --max-results 60 \
+    | jq -r '.UserPools[] | select( .Name == "'$USER_POOL_NAME'").Id')
+
+echo "Editing $userPoolFileName begin..."
+sed -e "s/<PoolName>/$USER_POOL_NAME/g" \
+  $userPoolFileName > edit/$userPoolFileName
+echo "Editing $userPoolFileName end\n"
+
+# if USER_POOL_NAME is aleady existed, this script don't create user pool
+if [ -z "$USER_POOL_ID" ]; then
+  # Create Cognito User Pool
+  echo "Creating Cognito User Pool $USER_POOL_NAME begin..."
+  USER_POOL_ID=$(aws cognito-idp create-user-pool --cli-input-json file://edit/$userPoolFileName | jq -r '.UserPool.Id')
+
+  echo "Identity Pool Id: $USER_POOL_ID"
+  echo "Creating Cognito Identity Pool $USER_POOL_NAME end\n"
+else
+  echo $USER_POOL_ID
+  echo "Using previous user pool with name $USER_POOL_NAME\n"
+fi
+
+# Create Cognito User Pool Client
+# find User pool client id
+USER_POOL_CLIENT_ID=$(aws cognito-idp list-user-pool-clients --user-pool-id $USER_POOL_ID \
+  --max-results 60 | jq -r '.UserPoolClients[] | select( .ClientName == "'$USER_POOL_CLIENT_NAME'").ClientId')
+
+echo "Editing $userPoolClientFileName begin..."
+sed -e "s/<UserPoolId>/$USER_POOL_ID/g" \
+  -e "s/<ClientName>/$USER_POOL_CLIENT_NAME/g" \
+  $userPoolClientFileName > edit/$userPoolClientFileName
+echo "Editing $userPoolClientFileName end\n"
+
+if [ -z "$USER_POOL_CLIENT_ID" ]; then
+  # Create Cognito User Pool
+  echo "Creating Cognito User Pool Client $USER_POOL_CLIENT_NAME begin..."
+  USER_POOL_CLIENT_ID=$(aws cognito-idp create-user-pool-client --cli-input-json file://edit/$userPoolClientFileName | jq -r '.UserPoolClient.ClientId')
+  echo "Identity Pool Id: $USER_POOL_CLIENT_ID"
+  echo "Creating Cognito Identity Pool $USER_POOL_CLIENT_NAME end\n"
+else
+  echo $USER_POOL_CLIENT_ID
+  echo "Using previous user pool with name $USER_POOL_CLIENT_NAME\n"
+fi
+
+# Init or Updating Cognito User Pool in config file
+if [ ! -f "config.json" ]; then
+  touch config.json
+  echo '{}' | jq '.' >> config.json
+fi
+
+mv config.json config.org
+jq '.'USER_POOL_ID'="'"$USER_POOL_ID"'"' config.org \
+  | jq '.'USER_POOL_CLIENT_ID'="'"$USER_POOL_CLIENT_ID"'"' > config.json
+rm config.org
+
+cd ..
---
+++ CognitoUserPool/bin/delete_cognito_user_pool.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Check if the AWS CLI is in the PATH
+found=$(which aws)
+if [ -z "$found" ]; then
+  echo "Please install the AWS CLI under your PATH: http://aws.amazon.com/cli/"
+  exit 1
+fi
+
+# Check if jq is in the PATH
+found=$(which jq)
+if [ -z "$found" ]; then
+  echo "Please install jq under your PATH: http://stedolan.github.io/jq/"
+  exit 1
+fi
+
+# Check dir
+if [ ! -d "CognitoUserPool" ]; then
+  echo "Please execute the command in the parent directory of CognitoUserPool"
+  exit 1
+fi
+
+cd CognitoUserPool
+
+# Check config.json file
+if [ ! -f "config.json" ]; then
+  echo "Please set config.json file"
+  exit 1
+fi
+
+# check USER_POOL_ID
+USER_POOL_ID=$(cat config.json | jq -r '.USER_POOL_ID')
+if [ -z "$USER_POOL_ID" ]; then
+  echo "Please set USER_POOL_ID in config.json"
+  exit 1
+fi
+
+# delte User Pool Id
+echo "User Pool Id: $USER_POOL_ID"
+echo "Removing Cognito User Pool $USER_POOL_ID begin"
+aws cognito-idp delete-user-pool --user-pool-id $USER_POOL_ID
+echo "Removing Cognito Identity Pool $USER_POOL_ID end"
+
+# delte USER_POOL_ID in config
+rm config.json
+echo '{}' | jq '.' >> config.json
